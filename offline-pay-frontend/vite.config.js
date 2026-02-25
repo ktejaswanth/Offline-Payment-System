3465c83761127ffb23f615878b531c80234413a1
KONDAVETI TEJASWANTH <2300031638@kluniversity.in>
2026-02-25 08:27:50 +0530

    Update vite.config.js

--- offline-pay-frontend/vite.config.js
+++ offline-pay-frontend/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     // Proxy API calls to backend during development
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://offline-payment-system-backend.onrender.com',
         changeOrigin: true,
       }
     }
